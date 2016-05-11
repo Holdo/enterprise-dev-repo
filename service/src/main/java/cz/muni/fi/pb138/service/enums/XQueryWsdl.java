c8616197a98968eafeaf716663c6a47fc2382fef
Peter Hutta <hutta.peto@gmail.com>
2016-05-11 16:34:05 +0200

    Basic XQuery operations

---
+++ service/src/main/java/cz/muni/fi/pb138/service/enums/XQueryWsdl.java
@@ -0,0 +1,46 @@
+package cz.muni.fi.pb138.service.enums;
+
+/**
+ * @author Peter Hutta
+ * @version 1.0  11.5.2016
+ */
+public enum XQueryWsdl {
+    GET_OPERATIONS("declare variable $file as xs:string external;" +
+            "for $op in distinct-values(doc($file)//*[name()='operation']/@name)" +
+            "return data($op)"),
+
+    GET_OPERATION_REQUEST("declare namespace functx = \"http://www.functx.com\";" +
+            "declare function functx:index-of-string-first" +
+            "  ( $arg as xs:string? , $substring as xs:string ) as xs:integer? {" +
+            "     if (contains($arg, $substring))" +
+            "     then string-length(substring-before($arg, $substring))+1" +
+            "     else (0)" +
+            "  }; (: used to trim possible namespace :)" +
+            "declare variable $file as xs:string external;" +
+            "declare variable $oper as xs:string external;" +
+            "for $rq in doc($file)//*[name()='operation' and @name=$oper]/*[name()='input']/@message" +
+            "return fn:substring(data($rq),functx:index-of-string-first(data($rq),':')+1)"),
+
+    GET_OPERATION_RESPONSE("declare namespace functx = \"http://www.functx.com\";" +
+            "declare function functx:index-of-string-first" +
+            "  ( $arg as xs:string? , $substring as xs:string ) as xs:integer? {" +
+            "     if (contains($arg, $substring))" +
+            "     then string-length(substring-before($arg, $substring))+1" +
+            "     else (0)" +
+            "  }; (: used to trim possible namespace :)" +
+            "declare variable $file as xs:string external;" +
+            "declare variable $oper as xs:string external;" +
+            "for $rs in doc($file)//*[name()='operation' and @name=$oper]/*[name()='output']/@message" +
+            "return fn:substring(data($rs),functx:index-of-string-first(data($rs),':')+1)");
+
+    private final String text;
+
+    XQueryWsdl(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
+}
---
+++ service/src/main/java/cz/muni/fi/pb138/service/enums/XQueryXsd.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pb138.service.enums;
+
+/**
+ * @author Peter Hutta
+ * @version 1.0  11.5.2016
+ */
+public enum XQueryXsd {
+    GET_COMPLEX_TYPES("declare variable $file as xs:string external;" +
+            "for $tname in distinct-values(doc($file)//*[name()='complexType']/../@name)" +
+            "return data($tname)"),
+
+    GET_SIMPLE_TYPES("declare variable $file as xs:string external;" +
+            "for $tname in distinct-values(doc($file)//*[name()='simpleType']/../@name)" +
+            "return data($tname)"),
+
+    GET_ELEMENTS("declare variable $file as xs:string external;" +
+            "for $elem in distinct-values(doc($file)//*[name()='element']/@name)" +
+            "return data($elem)"),
+
+    GET_ATTRIBUTES("declare variable $file as xs:string external;" +
+            "for $attr in distinct-values(doc($file)//*[name()='attribute']/@name)" +
+            "return data($attr)");
+
+    private final String text;
+
+    XQueryXsd(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
+}
