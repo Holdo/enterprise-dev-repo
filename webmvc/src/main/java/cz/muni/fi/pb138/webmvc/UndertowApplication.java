46251dcdc67d44dba11a1a0ed08cd8c12a37c606
root <root@tomas-ThinkPad-E550>
2016-05-13 15:13:10 +0200

    import added

--- webmvc/src/main/java/cz/muni/fi/pb138/webmvc/UndertowApplication.java
+++ webmvc/src/main/java/cz/muni/fi/pb138/webmvc/UndertowApplication.java
@@ -1,11 +1,14 @@
 package cz.muni.fi.pb138.webmvc;
 
 
+import cz.muni.fi.pb138.webmvc.controller.WebSocketConfig;
 import org.basex.BaseXServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {"cz.muni.fi.pb138"})
+@Import(WebSocketConfig.class)
 public class UndertowApplication {
 
 	public static void main(String[] args) throws Exception {
