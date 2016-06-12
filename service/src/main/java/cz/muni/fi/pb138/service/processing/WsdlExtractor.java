236f66a916eeb651a9c7701cc54bb644e8d0f067
Holdo <holic.michal@gmail.com>
2016-06-12 19:11:19 +0200

    Removed template headers

--- service/src/main/java/cz/muni/fi/pb138/service/processing/WsdlExtractor.java
+++ service/src/main/java/cz/muni/fi/pb138/service/processing/WsdlExtractor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pb138.service.processing;
 
 import cz.muni.fi.pb138.service.processing.entity.PathVersionPair;
