6555fada6169db653f4be0f951353ee3a6fb417d
Holdo <holic.michal@gmail.com>
2016-06-23 13:13:42 +0200

    Removed after-merge bullshit

--- service/src/main/java/cz/muni/fi/pb138/api/FileService.java
+++ service/src/main/java/cz/muni/fi/pb138/api/FileService.java
@@ -16,138 +16,94 @@ import java.util.zip.DataFormatException;
  * @author Ondřej Gasior
  */
 public interface FileService {
-    
-    /**
-     * Create - saved as version 1, Update  - saved as current version + 1
-     * @param fullPath fullpath to file (namespace/file.xxx)
-     * @param fileBytes binary representation of file
-     * @throws java.io.IOException
-     */
-    void saveFile(String fullPath, byte[] fileBytes) throws IOException, SAXException, ParserConfigurationException, DataFormatException, JAXBException;
 
-    /**
-<<<<<<< HEAD
-     * 
-     * @param fullPath fullpath to file (namespace/file.xxx)
-=======
-     * Deletes lastest version of specified file
-     * @param fullPath - fullpath to file (namespace/file.xxx)
->>>>>>> refs/remotes/origin/master
-     * @throws java.io.IOException
-     */
-    void deleteFile(String fullPath) throws IOException ;
+	/**
+	 * Create - saved as version 1, Update  - saved as current version + 1
+	 *
+	 * @param fullPath  fullpath to file (namespace/file.xxx)
+	 * @param fileBytes binary representation of file
+	 * @throws java.io.IOException
+	 */
+	void saveFile(String fullPath, byte[] fileBytes) throws IOException, SAXException, ParserConfigurationException, DataFormatException, JAXBException;
 
-    /**
-<<<<<<< HEAD
-     * 
-     * @param fullPath fullpath to file (namespace/file.xxx)
-     * @param version number higher than 0, specifies version to be deleted
-=======
-     * Deletes file specified by its name and version
-     * @param fullPath - fullpath to file (namespace/file.xxx)
-     * @param version - number higher than 0, specifies version to be deleted
->>>>>>> refs/remotes/origin/master
-     * @throws java.io.IOException
-     */
-    void deleteFile(String fullPath, int version) throws IOException ;
+	/**
+	 * Deletes the last version of specified file
+	 *
+	 * @param fullPath fullpath to file (namespace/file.xxx)
+	 * @throws java.io.IOException
+	 */
+	void deleteFile(String fullPath) throws IOException;
 
-    /**
-<<<<<<< HEAD
-     * 
-     * @param fullPath fullpath to file (namespace/file.xxx)
-     * @return binary representation of LAST VERSION of required file
-=======
-     * Returns file in binary format by its name
-     * @param fullPath - fullpath to file (namespace/file.xxx)
-     * @return - binary representation of LAST VERSION of required file
->>>>>>> refs/remotes/origin/master
-     * @throws java.io.IOException
-     */
-    byte[] getFileByFullPath(String fullPath)  throws IOException;
+	/**
+	 * Deletes file specified by its fullPath and version
+	 *
+	 * @param fullPath fullpath to file (namespace/file.xxx)
+	 * @param version  number higher than 0, specifies version to be deleted
+	 * @throws java.io.IOException
+	 */
+	void deleteFile(String fullPath, int version) throws IOException;
 
-    /**
-<<<<<<< HEAD
-     * 
-     * @param fullPath fullpath to file (namespace/file.xxx)
-     * @param version number higher than 0, specifies version of required file
-     * @return binary representation required of file
-=======
-     * Returns file in binary format by its name and version
-     * @param fullPath - fullpath to file (namespace/file.xxx)
-     * @param version - number higher than 0, specifies version of required file
-     * @return - binary representation required of file
->>>>>>> refs/remotes/origin/master
-     * @throws java.io.IOException
-     */
-    byte[] getFileByFullPathAndVersion(String fullPath, int version) throws IOException;
+	/**
+	 * Turns file in binary format by its fullPath
+	 *
+	 * @param fullPath fullpath to file (namespace/file.xxx)
+	 * @return binary representation of LAST VERSION of required file
+	 * @throws java.io.IOException
+	 */
+	byte[] getFileByFullPath(String fullPath) throws IOException;
 
-    /**
-<<<<<<< HEAD
-     *
-     * @param namespace "/" for ALL, specify namaspace to filter
-     * @param allVersions true for all versions, false for just the latest ones
-     * @param metaDatabase whether to list metaDatabase instead of document database
-=======
-     * Lists all files in specified namespace
-     * @param namespace - "/" for ALL, specify namaspace to filter
-     * @param allVersions - true for all versions, false for just the latest ones
-     * @param metaDatabase -  whether to list metaDatabase instead of document database
->>>>>>> refs/remotes/origin/master
-     * @return files as PathVersionPair
-     * @throws java.io.IOException
-     */
-    List<VersionedFile> listAllFiles(String namespace, boolean allVersions, boolean metaDatabase) throws IOException;
+	/**
+	 * Returns file in binary format by its name and version
+	 *
+	 * @param fullPath fullpath to file (namespace/file.xxx)
+	 * @param version  number higher than 0, specifies version of required file
+	 * @return binary representation required of file
+	 * @throws java.io.IOException
+	 */
+	byte[] getFileByFullPathAndVersion(String fullPath, int version) throws IOException;
 
-    /**
-<<<<<<< HEAD
-     * 
-     * @param namespace "/" for ALL, specify namaspace to filter
-     * @param allVersions true for all versions, false for just the latest ones
-     * @param metaDatabase whether to list metaDatabase instead of document database
-     * @param fileType One of supported file types like WAR, XSD, WSDL
-=======
-     * Lists all files in namespace non-recursively
-     * @param namespace - "/" for ALL, specify namaspace to filter
-     * @param allVersions - true for all versions, false for just the latest ones
-     * @param metaDatabase -  whether to list metaDatabase instead of document database
-     * @param fileType - One of supported file types like WAR, XSD, WSDL
->>>>>>> refs/remotes/origin/master
-     * @return files as PathVersionPair
-     * @throws java.io.IOException 
-     */
-    List<VersionedFile> listAllFilesByFileType(String namespace, boolean allVersions, boolean metaDatabase, FileType fileType) throws IOException;
+	/**
+	 * Lists all files in specified namespace
+	 *
+	 * @param namespace    "/" for ALL, specify namaspace to filter
+	 * @param allVersions  true for all versions, false for just the latest ones
+	 * @param metaDatabase whether to list metaDatabase instead of document database
+	 * @return files as PathVersionPair
+	 * @throws java.io.IOException
+	 */
+	List<VersionedFile> listAllFiles(String namespace, boolean allVersions, boolean metaDatabase) throws IOException;
+
+	/**
+	 * Lists all files in namespace non-recursively
+	 *
+	 * @param namespace    "/" for ALL, specify namaspace to filter
+	 * @param allVersions  true for all versions, false for just the latest ones
+	 * @param metaDatabase whether to list metaDatabase instead of document database
+	 * @param fileType     One of supported file types like WAR, XSD, WSDL
+	 * @return files as PathVersionPair
+	 * @throws java.io.IOException
+	 */
+	List<VersionedFile> listAllFilesByFileType(String namespace, boolean allVersions, boolean metaDatabase, FileType fileType) throws IOException;
 
 
-    /**
-<<<<<<< HEAD
-     *
-     * @param namespace "/" for ALL, specify namaspace to filter
-     * @param allVersions true for all versions, false for just the latest ones
-     * @param metaDatabase whether to list metaDatabase instead of document database
-     * @param fileType One of supported file types like WAR, XSD, WSDL
-=======
-     * List all files in namespace recursively
-     * @param namespace - "/" for ALL, specify namaspace to filter
-     * @param allVersions - true for all versions, false for just the latest ones
-     * @param metaDatabase -  whether to list metaDatabase instead of document database
-     * @param fileType - One of supported file types like WAR, XSD, WSDL
->>>>>>> refs/remotes/origin/master
-     * @return files as PathVersionPair
-     * @throws java.io.IOException
-     */
-    List<VersionedFile> listAllFilesByFileTypeRecursively(String namespace, boolean allVersions, boolean metaDatabase, FileType fileType) throws IOException;
+	/**
+	 * List all files in namespace recursively
+	 *
+	 * @param namespace    "/" for ALL, specify namaspace to filter
+	 * @param allVersions  true for all versions, false for just the latest ones
+	 * @param metaDatabase whether to list metaDatabase instead of document database
+	 * @param fileType     One of supported file types like WAR, XSD, WSDL
+	 * @return files as PathVersionPair
+	 * @throws java.io.IOException
+	 */
+	List<VersionedFile> listAllFilesByFileTypeRecursively(String namespace, boolean allVersions, boolean metaDatabase, FileType fileType) throws IOException;
 
-    /**
-<<<<<<< HEAD
-     * 
-     * @param fullPath fullpath to file (namespace/file.xxx)
-     * @return versions of fullpath specified file
-=======
-     * List all versions of specified file
-     * @param fullPath - fullpath to file (namespace/file.xxx)
-     * @return - versions of fullpath specified file
->>>>>>> refs/remotes/origin/master
-     * @throws java.io.IOException
-     */
-    List<Integer> listFileVersions(String fullPath) throws IOException;
+	/**
+	 * List all versions of specified file
+	 *
+	 * @param fullPath fullpath to file (namespace/file.xxx)
+	 * @return versions of fullpath specified file
+	 * @throws java.io.IOException
+	 */
+	List<Integer> listFileVersions(String fullPath) throws IOException;
 }
